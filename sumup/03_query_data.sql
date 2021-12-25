1286ef78d1f737708ee6906480c5fbb67fed1402
Petra Carrion <petra@carrion.io>
2021-12-25 17:40:04 +0100

    Sumup

--- sumup/03_query_data.sql
+++ sumup/03_query_data.sql
@@ -5,12 +5,12 @@ order by order_timestamp desc
 limit 1;
 
 select restaurant_id, sum(turnover)
-from orders
+from agg_orders
 where order_date = '2021-06-21'
 group by restaurant_id;
 
 select country, sum(turnover)
-from orders
-         join restaurants r on orders.restaurant_id = r.restaurant_id
+from agg_orders o
+         join restaurants r on o.restaurant_id = r.restaurant_id
 where order_date = '2021-06-21'
 group by country;
